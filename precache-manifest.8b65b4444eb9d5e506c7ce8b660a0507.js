b88d40f0b31dc1ea480ec1f67aa8ba0b5110dc29
jortle <thejoshguyer@gmail.com>
2019-11-13 23:13:40 -0600

    Updates

---
+++ precache-manifest.8b65b4444eb9d5e506c7ce8b660a0507.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0abb2674b0634ffabb02a78f18bc6a59",
+    "url": "/recipe-project/index.html"
+  },
+  {
+    "revision": "e97260f712b6580f4505",
+    "url": "/recipe-project/static/css/main.90fcd890.chunk.css"
+  },
+  {
+    "revision": "dc52c575058207fe9817",
+    "url": "/recipe-project/static/js/2.7341d820.chunk.js"
+  },
+  {
+    "revision": "e97260f712b6580f4505",
+    "url": "/recipe-project/static/js/main.8067eb81.chunk.js"
+  },
+  {
+    "revision": "70fdaae7ee9b6ccdfb1d",
+    "url": "/recipe-project/static/js/runtime-main.498c736d.js"
+  }
+]);
